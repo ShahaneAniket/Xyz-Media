66363c5bf7c0892f3e1257252c42e60c53342ac8
Aniket Shahane <aniket.shahane@prosot.com>
2021-05-14 22:31:41 +0530

    Pages Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { BodyComponent } from './body/body.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [ 
+  { path: '', component: BodyComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'login', component: LoginComponent },
+   
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
